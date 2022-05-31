29b455de88b23a6ece4ca44ccdd564913a8e9b67
franco <francosimonazzi@hotmail.com>
2022-05-31 17:38:59 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3497e962c5e17a1e246469fe725ee81d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '48b58c62-1d9c-4583-a1a8-4869519fa422'
+};
